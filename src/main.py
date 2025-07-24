27727029afbe046c9a5a6c101c6c3cbcda209e4a
Robert Ngobeni <rngobeni1@icloud.com>
2025-07-24 16:49:28 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,21 +1,24 @@
 import config
-# from pathlib import Path
+from pathlib import Path
 from modules.mkdir import MakeFolder
 from modules.passwordApp import PasswordProtectApp
 from modules.encrypt_file import EncryptFile
 
 if __name__ == "__main__":
-    folder_list = [
-        config.IMPORT_FILES,
-        config.TO_BE_PROCESSED,
-        config.PROCESSED,
-        config.NOT_PROCESSED,
-        config.PASSWORD_FOLDER,
-        config.LOGS_FOLDER
-        ]
+    try:
+            
+        folder_list = [
+            config.IMPORT_FILES,
+            config.TO_BE_PROCESSED,
+            config.PROCESSED,
+            config.NOT_PROCESSED,
+            config.PASSWORD_FOLDER
+            ]
 
-    for folder in folder_list:
-        MakeFolder(str(folder)).create_directory()
+        for folder in folder_list:
+            MakeFolder(str(folder)).create_directory()
 
-    PasswordProtectApp().create_app()
-    
+        PasswordProtectApp().create_app()
+        
+    except Exception as e:
+        print(f"Error: {e}")
